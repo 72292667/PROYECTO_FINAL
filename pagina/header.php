5d5f2f0b316a48082468bee29c56c63549e867ad
72292667 <1005020181@unajma.edu.pe>
2021-01-19 01:16:51 -0500

    avance 3.1

--- pagina/header.php
+++ pagina/header.php
@@ -1,6 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Delivery Andahuylas </title>
+  <!--  trabajamos con un formualrio boostrap-->
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
 
+  </head>
+  <div>
+    <footer>
+
+    </footer>
+  </div>
+</html>
 <header>
-  <a href="tom">Delivey Andahuylas</a>
+  
 </header>
 
 
