e288cf880b0a1008a28c547b8dc6aa63fd51000b
72292667 <1005020181@unajma.edu.pe>
2021-01-19 01:20:19 -0500

    avance 3.2

--- pagina/header.php
+++ pagina/header.php
@@ -8,7 +8,25 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <style>
+      <style>
+    /* estilos  */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+
+    .carousel-inner img {
+      width: 100%; /* la imagen al 100% */
+      min-height: 200px;
+    }
+
+    /* Ocultar el texto del carrusel cuando la pantalla tiene menos de 600 píxeles de ancho*/
+    @media (max-width: 600px) {
+      .carousel-caption {
+        display: none; 
+      }
+    }
+  </style>
 
   </head>
   <div>
