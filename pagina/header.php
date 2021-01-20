a851dc28e3383e8e9aeb78f4e8954388f00e5a44
72292667 <1005020181@unajma.edu.pe>
2021-01-19 22:06:00 -0500

    version 3.5

--- pagina/header.php
+++ pagina/header.php
@@ -53,9 +53,178 @@
     </div>
   </nav>
 
+  <div class="container">
+  <div class="row">
+    <div class="col-sm-8">
+     <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      
+      <!-- Indicadores -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+      </ol>
 
+      <!--  fotos para diapositivas-->
+      <!--  trabajamos con un formualrio boostrap-->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="../pagina/img/hamburguesa.png" alt="Image">
+          <div class="carousel-caption">
+            <h3> S/. 10.00 </h3>
+            <p>Hamburguesa <p>
+          </div>      
+        </div>
 
-    <footer>
+        <div class="item">
+          <img src="../pagina/img/pizza.png" alt="Image">
+          <div class="carousel-caption">
+            <h3>S/. 20.00 </h3>
+            <p>pizza</p>
+          </div>      
+        </div>
+
+        <div class="item">
+          <img src="../pagina/img/pollo.jpg" alt="Image">
+          <div class="carousel-caption">
+            <h3>S/. 40.00</h3>
+            <p>Pollo ala Braza</p>
+          </div>      
+        </div>
+        <div class="item">
+          <img src="../pagina/img/anticucho.jpg" alt="Image">
+          <div class="carousel-caption">
+            <h3>S/. 15.00</h3>
+            <p>Anticucho Vejetariano</p>
+          </div>      
+        </div>
+        <div class="item">
+          <img src="../pagina/img/chaufa.jpg" alt="Image">
+          <div class="carousel-caption">
+            <h3>S/. 18.00</h3>
+            <p>Chaufa de Pollo</p>
+          </div>      
+        </div>
+
+      </div>
+         <!-- Controles izquierdo y derecho -->
+         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="well">
+      <p>HAMBURGUESAS</p>
+    </div>
+    <div class="well">
+       <p>PIZZA</p>
+    </div>
+    <div class="well">
+       <p>COMIDA CHINA</p>
+    </div>
+    <div class="well">
+       <p>POLLO A LA BRAZA</p>
+    </div>
+    <div class="well">
+       <p>ANTICUCHOS</p>
+    </div>
+  </div>
+</div>
+<hr>
+</div>
+
+<div class="container text-center">    
+  <h1>Menu Hamburguesas</h1>
+  <br>
+  <div class="row">
+    <div class="col-sm-3">
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div>
+    <div class="col-sm-3"> 
+      <img src="../pagina/img/hamburguesa.png"class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>  
+    </div>
+    <div class="col-sm-3">
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div>
+    <div class="col-sm-3">
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div>
+  </div>
+</div>
+
+<div class="container text-center">    
+  <h1>Menu Pollos</h1>
+  <br>
+  <div class="row">
+    <div class="col-sm-2">
+      <img src="../pagina/img/pollo.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div>
+    <div class="col-sm-2"> 
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>  
+    </div>
+    <div class="col-sm-2"> 
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div>
+    <div class="col-sm-2"> 
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div> 
+    <div class="col-sm-2"> 
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div>     
+    <div class="col-sm-2"> 
+      <img src="../pagina/img/hamburguesa.png" class="img-responsive" style="width:100%" alt="Image">
+      <button type="button" class="btn btn-primary btn-lg">Agregar</button>
+    </div> 
+  </div>
+</div><br>
+
+<footer class="bg-light text-center text-lg-start">
+
+  <div class="container p-4">
+ 
+    <div class="row">
+      
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Delyvery Andahuaylas</h5>
+        <p>
+            servicios de Delivery Andahuaylas, ofrece todo tipo de comidas, y 
+            de la mejor calidad, nuestro servicio es rápido y eficiente, tomando 
+            en cuenta los más altos estándares internacionales en cuanto a control 
+            de calidad de todos los productos que se ofrece y con el debido protocolo de seguridad.
+        </p>
+      </div>
+  
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Nuestro Servicio</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+            <a href="Promociones.php" class="text-dark">Promociones</a>
+            </li>
+            <li>
+            <a href="nosotros.php" class="text-dark">Nosotros</a>
+            </li>
+            <li>
+            <a href="contacto.pho" class="text-dark">Contacto</a>
+           </li>
+          </ul>
+       </div>
+      </div>
+    </div>
 
     </footer>
   </body>
