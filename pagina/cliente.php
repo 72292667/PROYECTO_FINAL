91ca3563e9d530eaf23ad4c8ff2fd67b29657dd9
72292667 <1005020181@unajma.edu.pe>
2021-02-01 16:16:50 -0500

    pedidos

--- pagina/cliente.php
+++ pagina/cliente.php
@@ -2,55 +2,6 @@
   session_start();
   include ('../database.php');
   $conexion = new mysqli($server, $username, $password, $database);
-/* 
-	
-	$id = $_SESSION['Usuario'];
-	$sqlid = "SELECT id_usu FROM usuario WHERE id_usu='".$id."'";
-	$resul = mysql_query($sqlid) or die ("Error" .mysql_error());
-	$sql = "INSERT INTO cliente VALUES('','".$nombre."','".$apellidos."','".$direccion."','".$celular."','".$resul."')";
-  $resultado = mysql_query($sql) or die ("Error" .mysql_error());
-  
-*/
-
-
-
-
-/* 
-  $conexion = new mysqli($server, $username, $password, $database);
-
-    $user = $_SESSION['Usuario'];
-    $query = "SELECT id_usu FROM usuario WHERE id_usu = '$user'";
-    $result = $conexion->query($query);
-    if (!$result) die ("Falló el acceso a la base de datos"); 
-    
-    $rows = $result->num_rows;
-
-     if (isset($_POST["nombre"]) && isset($_POST["apellidos"]) && isset($_POST["direccion"])&& isset($_POST["celular"])) {
-       $nombre = get_post($conexion,'nombre');
-       $apellidos = get_post($conexion,"apellidos");
-       $direccion = get_post($conexion,"direccion");
-       $celular = get_post($conexion,"celular");
-
-       $query = "INSERT INTO cliente  (id_cli, nombre, apellidos, direccion, celular, id_usu) VALUES ('','$nombre', '$apellidos', '$direccion', '$celular', '')";
-
-       $result = $conexion->query($query);
-       if ($conexion->connect_error) die ("Fatal error");
-       if (!$result) 
-       echo "registro falló <br><br>";
-
-      }
- 
-   function mysql_fix_string($coneccion, $string)
-   {
-     if (get_magic_quotes_gpc())
-        $string = stripcslashes($string);
-     return $coneccion->real_escape_string($string);
-   }
-   function get_post($con, $var)
-   {
-     return $con->real_escape_string($_POST[$var]);
-   }
-   */
 
 
   $user = $_SESSION['Usuario'];
@@ -71,7 +22,9 @@
     $stmt->bindParam(':celular', $_POST['celular']);
   
     if ($stmt->execute()) {
-      $message = ' se registro correctamente';
+           
+      $message = 'se registro correctamente';
+      header("Location: ../pagina/header.html");
     } else {
       $message = 'Lo sentimos, sus datos no fueron registrados';
     }
@@ -80,29 +33,122 @@
 ?>
 <!DOCTYPE html>
 <html>
+
   <head>
-    <meta charset="utf-8">
-    <title>Regístrate</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="estilo/css/style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </head>
+
+  
   <body>
 
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Delivery Andahuylas</a>
+    </div>
+    
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="salir.php"><span class="glyphicon glyphicon-log-in"></span> Salir</a></li>
+      </ul>
+      </div>
+    </div>
+  </nav>
 
     <?php if(!empty($message)): ?>
       <p> <?= $message ?></p>
     <?php endif; ?>
 
-    <h1> Regístrate</h1>
+    <div class="image-fondo cabecera">
+      <br>
+    <h1> CONFIRMA TU DIRRECCIÓN DE ENTREGA</h1><br>
 
     <form action="cliente.php" method="POST">
-      <input name="nombre" type="text" placeholder="Ingrese su nombre">
-      <input name="apellidos" type="text" placeholder="Ingrese su apellidos">
-      <input name="direccion" type="text" placeholder="Ingrese su direccion">
-      <input name="celular" type="text" placeholder="Ingrese su celular">
+      <input name="nombre" type="text" placeholder="Ingrese su Nombre">
+      <input name="apellidos" type="text" placeholder="Ingrese su Apellidos">
+      <input name="direccion" type="text" placeholder="Ingrese su Dirección">
+      <input name="celular" type="text" placeholder="Número Celular">
 
-      <input type="submit" value="ENVIAR">
-    </form>
+      <input type="submit" value="CONFIRMAR" type="color">
+    </form><br>   
+
+    </div>
+
+    <style>
+            .image-fondo {
+              background-image: url("../pagina/img/fondo.jpg");
+              background-size: cover;
+              color: #0A0909;
+          }
+
+          .cabecera{
+            color: #9B0E1D;
+            height: 400px;
+            line-height: 50px;
+            text-align: center;
+            vertical-align: middle;
+            text-shadow: #111 0px 0px 10px;
+          }
+
+          .bg-light{
+            color: #0D9B19;  
+            vertical-align: middle;
+            text-shadow: #111 0px 0px 10px;        
+          }
+    </style> <br>
+
+    
+ 
+    <footer class="bg-light text-center text-lg-start">
+
+        <div class="container p-4">
+
+          <div class="row">
+        
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+              <h3 class="text-uppercase">Delyvery Andahuaylas</h3>
+              <p>
+                  Servicios de Delivery Thomy, ofrece todo tipo de comidas, y 
+                  de la mejor calidad, nuestro servicio es rápido y eficiente, tomando 
+                  en cuenta los más altos estándares internacionales en cuanto a control 
+                  de calidad de todos los productos que se ofrece y con el debido
+                  protocolo de seguridad.
+              </p>
+            </div>
+
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+         <h4 class="text-uppercase mb-0">encuentranos en:</h4>
+
+                <ul class="list-unstyled">
+                <li>
+                    <a href="https://web.facebook.com" class="text-dark">FACEBOOK</a>
+                </li>
+                <li>
+                    <a href="http://deliveryandahuaylas.rf.gd" class="text-dark">PÁGINA WEB</a>
+                </li>
+                </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+         <h4 class="text-uppercase mb-0">DELIVERY </h4>
+
+                <ul class="list-unstyled">
+                <li>
+                <img src="../pagina/img/logo.png" class="img-responsive" style="width:100px" alt="Image"><br><br>
+                </li>
+                
+                </ul>
+      </div>
+     </div>
+   </div>
+   </footer>
 
   </body>
 </html>
