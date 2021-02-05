7ba6d4bc9b483f7524c1e4e89737a6b5838740a9
72292667 <1005020181@unajma.edu.pe>
2021-02-04 20:02:39 -0500

    ultimos retoques

---
+++ estilo/js/funcion.js
@@ -0,0 +1,10 @@
+ //Activa campos para registrar cliente
+ $('.btn_new_cliente').click(function(e){
+    e.preventDefault();
+    $('#nom_cliente').removeAttr('disabled');
+    $('#apel_cliente').removeAttr('disabled');
+    $('#dir_cliente').removeAttr('disabled');
+    $('#tel_cliente').removeAttr('disabled');
+
+    $('#div_registro_cliente').slideDown();
+  });
